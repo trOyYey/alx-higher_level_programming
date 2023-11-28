6969042046689a5a96e2247ca2db048cccfc72ec
trOyYey <troyzuwara@gmail.com>
2023-11-27 18:56:16 -0800

    adding test functions in headerfile

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -14,6 +14,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
