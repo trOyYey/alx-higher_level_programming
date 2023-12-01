2dfad35df68ef1d93195aa449cc80b1589a12956
trOyYey <troyzuwara@gmail.com>
2023-11-30 18:51:22 -0800

    fixing premision and style for task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+        if name[:2] != '__':
             print(name)
