e37ea99cf6b22133f6705c8a55f95fab23579d61
trOyYey <troyzuwara@gmail.com>
2023-11-30 18:43:20 -0800

    fixing style

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def main():
     result = 0
     for number in argv[1:]:
         result += int(number)
     print(result)
+
 
 if __name__ == "__main__":
     main()
