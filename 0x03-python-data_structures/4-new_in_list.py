e21a7791dafe114ef67fb9fde651e3b998eb7a44
trOyYey <troyzuwara@gmail.com>
2023-12-04 18:32:52 -0800

    tasks 4 5 6

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if (idx < 0 or idx > len(my_list) - 1):
+        return new_list
+    clone_list = my_list.copy()
+    clone_list[idx] = element
+    return clone_list
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for z in matrix:
+        for y in z:
+            if (n == z[-1]):
+                print("{:d}".format(n), end='')
+            else:
+                print("{:d}".format(n), end=' ')
+        print()
