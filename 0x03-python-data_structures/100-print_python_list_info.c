09eb3466afc1a8247d23f7bffb26fdf11ef2f0d5
trOyYey <troyzuwara@gmail.com>
2023-12-05 19:19:43 -0800

    fixing include python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include <python.h>
+#include "python.h"
 
 /**
  * print_python_list_info - prints information about python lists
@@ -9,8 +9,8 @@ void print_python_list_info(PyObject *p)
 {
 	int total_count, allocation, itr;
 	PyObject *object;
-	const char *type
-	total_count = PyList_Size(p);
+
+	total_count = Py_SIZE(p);
 	allocation = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %d\n", total_count);
@@ -19,8 +19,8 @@ void print_python_list_info(PyObject *p)
 	for (itr = 0; itr < total_count; itr++)
 	{
 		printf("Element %d: ", itr);
+
 		object = PyList_GetItem(p, itr);
-		type = Py_TYPE(object)->tp_name;
-		printf("%s\n", type);
+		printf("%s\n", Py_TYPE(object)->tp_name);
 	}
 }
