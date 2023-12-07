5bd86d0f1aaaca1d5ff67774a76ab1eb2075ba1e
trOyYey <troyzuwara@gmail.com>
2023-12-06 18:19:44 -0800

    few more tasks

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary:
+        for j in sorted(a_dictionary):
+            print("{}: {}".format(j, a_dictionary[j]))
