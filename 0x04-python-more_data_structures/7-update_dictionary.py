6a5469fadf1e9a56e420afc1b0d8c406528588f3
trOyYey <troyzuwara@gmail.com>
2023-12-06 18:21:41 -0800

    fixing task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary:
-         a_dictionary.update({key: value})
+        a_dictionary.update({key: value})
         return a_dictionary
