4ce08805bdedcabe6a8f71f08e8c6e28fa7991eb
trOyYey <troyzuwara@gmail.com>
2023-12-06 18:47:36 -0800

    fixing task 11

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_String, str):
+        return 0
+    sum = 0
+    number = 0
+    lett = { 'I': 1, 'V': ,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
+    for i in reversed(roman_string):)
