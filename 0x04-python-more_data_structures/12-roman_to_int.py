3d8b74ab1eb1dfe389cfd670f831534b776ee09b
trOyYey <troyzuwara@gmail.com>
2023-12-08 15:40:19 -0800

    fixing style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
     sum = 0
     previous_v = 0
-    lett = { 'I': 1, 'V': 5,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    lett = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in reversed(roman_string):
         current_v = lett[i]
         if current_v < previous_v:
