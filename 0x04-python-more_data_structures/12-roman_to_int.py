6b2261b8bc71d850042e38c3182684876d10f61c
trOyYey <troyzuwara@gmail.com>
2023-12-08 15:36:32 -0800

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_String, str):
+    if not isinstance(roman_string, str):
         return 0
     sum = 0
-    number = 0
-    lett = { 'I': 1, 'V': ,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 100}
-    for i in reversed(roman_string):)
+    previous_v = 0
+    lett = { 'I': 1, 'V': 5,'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        current_v = lett[i]
+        if current_v < previous_v:
+            sum -= current_v
+        else:
+            sum += current_v
+        previous_v = current_v
+    return sum
