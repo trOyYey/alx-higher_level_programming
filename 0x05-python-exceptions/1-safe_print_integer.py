ebd3f2cd751373bc88c91a57cad5482c830ae0bc
trOyYey <troyzuwara@gmail.com>
2023-12-18 17:27:36 -0800

    task-01

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except TypeError:
+        return False
+    except ValueError:
+        return False
+    else:
+        return True
