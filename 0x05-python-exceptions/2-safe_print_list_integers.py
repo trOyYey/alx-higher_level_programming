07a9a29de3a56ec1c275ced43529f5477a30d21e
trOyYey <troyzuwara@gmail.com>
2023-12-18 17:49:08 -0800

    fixing variable name in task 02

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             y += 1
         except (TypeError, ValueError):
             None
-        y += 1
-
+        i += 1
     print()
     return y
