f0486474689cb8007a1153824ac489e87f518514
trOyYey <troyzuwara@gmail.com>
2023-12-18 17:42:17 -0800

    task-02

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    y, i = 0, 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            y += 1
+        except (TypeError, ValueError):
+            None
+        y += 1
+
+    print()
+    return y
