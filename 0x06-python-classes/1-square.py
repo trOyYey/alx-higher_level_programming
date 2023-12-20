61b244acd0eec361927e1ed271db0d77fb295663
trOyYey <troyzuwara@gmail.com>
2023-12-19 18:29:15 -0800

    task 02

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defining a Square."""
+    def __init__(self, size=0):
+        """initilazing the size value of Square"""
+        self.__size = size
