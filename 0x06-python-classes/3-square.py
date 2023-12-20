9c27ff86156288f1bf72470f3d7baad349499722
trOyYey <troyzuwara@gmail.com>
2023-12-19 18:40:36 -0800

    task03

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defining a Square."""
+    def __init__(self, size=0):
+        """initilazing the size value of Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area method returns current square area"""
+        return self.__size ** 2
