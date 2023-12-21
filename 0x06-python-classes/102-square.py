b0f38fd3a536d5f794d7560fdbb261d28006cc32
trOyYey <troyzuwara@gmail.com>
2023-12-20 18:47:20 -0800

    fixing pycodestyle

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -17,22 +17,22 @@ class Square:
         return self.__size ** 2
 
     def __lt__(self, other):
-            return self.area() < other.area()
+        return self.area() < other.area()
 
     def __gt__(self, other):
-            return self.area() > other.area()
+        return self.area() > other.area()
 
     def __le__(self, other):
-            return self.area() <= other.area()
+        return self.area() <= other.area()
 
     def __ge__(self, other):
-            return self.area() >= other.area()
+        return self.area() >= other.area()
 
     def __ne__(self, other):
-            return self.area() != other.area()
+        return self.area() != other.area()
 
     def __eq__(self, other):
-            return self.area() == other.area()
+        return self.area() == other.area()
 
     @property
     def size(self):
