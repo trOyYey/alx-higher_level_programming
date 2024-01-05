884950ae6bb1e92870fb032899a3af2f42164d9b
trOyYeyY <troyzuwara@gmail.com>
2024-01-05 01:42:32 +0000

    task 02

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """Representation of the Rectangle"""
+    def __init__(self, width=0, height=0):
+        """initilationg of the rectangle with the height and width"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """returns the value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setting width value"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setting height value"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculates the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """calculates the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__width * 2 + (self.__height * 2)
