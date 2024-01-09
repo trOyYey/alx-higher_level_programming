b2cf74daf433283cedd636463750764d720aa759
trOyYeyY <troyzuwara@gmail.com>
2024-01-09 01:55:38 +0000

    task03

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""method for is kind of class module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """object is same or inherited from object"""
+    return isinstance(obj, a_class)
