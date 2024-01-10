f1df0423257bc051f2139cc00e1159fce28dbe94
trOyYeyY <troyzuwara@gmail.com>
2024-01-10 01:50:13 +0000

    task 00

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""def read_file mod"""
+
+
+def read_file(filename=""):
+    """prints all content of a specified file"""
+    with open(filename, encoding='utf-8') as datafile:
+        print(datafile.read(), end="")
