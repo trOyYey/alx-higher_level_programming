76a1fcf5b24c1194b4fd87700aec41cb082661cd
trOyYeyY <troyzuwara@gmail.com>
2024-01-10 02:44:32 +0000

    testing task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""read from json, add the arguments, save it in file"""
-
-
 import sys
-load_from_json = __import__('6-load_from_json_file').load_from_json_file
-save_to_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
 
 
 def main():
-    filename = "add_item.json"
+    fileName = "add_item.json"
     try:
-        lst = load_from_json(filename)
+        lis = load_json(fileName)
     except FileNotFoundError:
-        lst = []
+        lis = []
 
-    for i in sys.argv[1:]:
-        lst.append(i)
+    for a in sys.argv[1:]:
+        lis.append(a)
 
-    save_to_json(lst, filename)
+    save_json(lis, fileName)
 
 
 if __name__ == "__main__":
