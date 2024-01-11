b09b0fdbb3d956ecd79a5f91938b9d5ddf36be0a
trOyYeyY <troyzuwara@gmail.com>
2024-01-11 01:23:32 +0000

    Technical interview preparation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,14 +2,14 @@
 """Defining pascal_triangle funtion"""
 
 
-def generate_pascal_triangle(rows):
+def pascal_triangle(n):
     """Generates Pascal's triangle as a list of lists"""
-    if rows <= 0:
+    if n <= 0:
         return []
 
     triangle = [[1]]
 
-    for x in range(rows - 1):
+    for x in range(n - 1):
         new_row = [1]
         for col in range(len(triangle[-1]) - 1):
             new_row.append(triangle[-1][col] + triangle[-1][col + 1])
