b11c78d9babf83f512bf65d295e51af2b5c75757
trOyYeyY <troyzuwara@gmail.com>
2024-01-15 22:59:57 +0000

    task01

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base class mod"""
+
+
+class Base:
+    """base class for all geometric forms"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
