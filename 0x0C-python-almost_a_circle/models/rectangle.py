765ffda49161c3cba7ff6e84a7bee8bbbebc5550
trOyYeyY <troyzuwara@gmail.com>
2024-01-15 23:33:36 +0000

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,12 +7,22 @@ class Rectangle(Base):
     '''rectangle class'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''constructor'''
+        '''constructor of the rect'''
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        '''width of this rect'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validate_attributes("width", value, False)
+        self.__width = value
 
     @property
     def height(self):
@@ -21,18 +31,8 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        self.validate_attributes("height", value)
+        self.validate_attributes("height", value, False)
         self.__height = value
-
-    @property
-    def width(self):
-        '''width of the rect'''
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.validate_attributes("width", value)
-        self.__width = value
 
     @property
     def x(self):
