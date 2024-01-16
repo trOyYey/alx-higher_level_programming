bf7849dc5dfe85a773b746c1a203075c8d4e2281
trOyYeyY <troyzuwara@gmail.com>
2024-01-16 01:19:28 +0000

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,14 @@ class Base:
         lst = [x.to_dictionary() for x in list_objs] if list_objs else []
         with open(f"{name}.json", "w+", encoding="utf-8") as file:
             file.write(Base.to_json_string(lst))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """"dict to json"""
+        tmp = json_string
+        if tmp is None or type(tmp) is not str:
+            return []
+        elif len(tmp) == 0:
+            return []
+        else:
+            return json.loads(json_string)
