1afa7eec7c8ebb1905e5cf1ddd03cd7b4bd79ae2
trOyYeyY <troyzuwara@gmail.com>
2024-01-16 00:38:01 +0000

    task 12 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,3 +28,26 @@ class Square(Rectangle):
         X = self.x
         Y = self.y
         return "[Square] ({}) {}/{} - {}".format(ID, X, Y, SIZE)
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''private method that updates instance attributes by **args.'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''updates instance attributes by no-keyword & -or- keyword arguments.'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """returns the square as a dictionary"""
+        return {"id": self.id, "size": self.size,
+                "x": self.x, "y": self.y}
