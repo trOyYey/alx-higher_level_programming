779bbceea9ef55d2489ac87001512d8bbbfc5fae
trOyYeyY <troyzuwara@gmail.com>
2024-02-16 01:01:47 +0000

    fixing and pushing last task

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- lists average temps by city in descending order
-SELECT city, AVG(value) as average_temps
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average_temps DESC
+ORDER BY avg_temp DESC
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 --lists the top 3 temps dwuring august and july in descending order
-SELECT city, AVG(value) as av_temps
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 where month = 8 OR month = 7
 GROUP BY city
-ORDER BY av_temps DESC
+ORDER BY avg_temp DESC
 LIMIT 3
