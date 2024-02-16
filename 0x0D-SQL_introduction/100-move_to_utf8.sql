6621728479741d00bcfed5f9ced300a3472487a1
trOyYeyY <troyzuwara@gmail.com>
2024-02-16 00:33:35 +0000

    changing syntax

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,5 @@
 -- convert hbtn to UTF8
-ALTER DATABASE hbtn_0c_0
-MODIFY CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- convert first_table to UTF8
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER
-SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
