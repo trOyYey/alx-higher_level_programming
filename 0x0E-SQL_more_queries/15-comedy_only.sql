14d8e6a1ab7ebaefc3ca6ea1e22c4638a0efcec0
trOyYeyY <troyzuwara@gmail.com>
2024-02-16 01:47:39 +0000

    fixing syntax

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- lists comedy shows from database
-SELECT show.title 
+SELECT show_a.title 
 FROM tv_genres AS tv_g
 	INNER JOIN tv_show_genres AS tv_show_g
 	ON tv_g.id = tv_show_g.genre_id 
-	INNER JOIN tv_shows AS shows_a
+	INNER JOIN tv_shows AS show_a
 	ON tv_show_g.show_id = show_a.id 
 WHERE tv_g.name = 'Comedy' 
 ORDER BY show_a.title ASC;
