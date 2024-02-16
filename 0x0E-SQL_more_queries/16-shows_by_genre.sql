4e83547fcb9568b82e40021246273ceb62288e99
trOyYeyY <troyzuwara@gmail.com>
2024-02-16 01:45:48 +0000

    fixing syntax

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows and all genres linked to specified show
-SELECT show.title, genre.name
+SELECT show_s.title, tv_g.name
 FROM tv_shows AS show_s
 	LEFT JOIN tv_show_genres AS show_g
 	ON show_s.id = show_g.show_id
