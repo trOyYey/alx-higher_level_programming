9d12b6ec299ddf3e1dfe6c38d2e9473c08a4e7f6
trOyYeyY <troyzuwara@gmail.com>
2024-03-11 23:35:23 +0000

    fixing semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const argc = process.argv.length;
 if (argc === 3) {
   console.log('Argument found');
 } else if (argc > 3) {
- console.log('Arguments found');
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
