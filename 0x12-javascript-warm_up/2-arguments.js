ffc6b31025fa641f17ae2b18d58a790e25aeee42
trOyYeyY <troyzuwara@gmail.com>
2024-03-11 23:34:16 +0000

    multi langs and arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argc = process.argv.length;
+
+if (argc === 3) {
+  console.log('Argument found');
+} else if (argc > 3) {
+ console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
