35c39aefc69b3ec235f4478776885d0b1d6d72fb
trOyYeyY <troyzuwara@gmail.com>
2024-03-13 01:49:03 +0000

    task 9 10 and 1 advanced

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return (n) => { return n.toString(base); };
+};
---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const list = require('./100-data.js').list;
+const mylist = list.map((x, i) => x * i);
+console.log(list);
+console.log(mylist);
