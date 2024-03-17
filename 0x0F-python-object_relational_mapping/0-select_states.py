c052422bd0bf8510ca03389e01bfc9fbf237e890
trOyYeyY <troyzuwara@gmail.com>
2024-03-17 01:23:51 +0000

    fixing indentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,11 @@
 """ Getting all states using mysqldb"""
 import MySQLdb
 from sys import argv
-my_host = "localhost"
+host = "localhost"
 
 
 def main():
-    db = MySQLdb.connect(host=my_host, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host=host, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id;")
     rows = cur.fetchall()
@@ -15,5 +15,6 @@ def main():
     cur.close()
     db.close()
 
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     main()
