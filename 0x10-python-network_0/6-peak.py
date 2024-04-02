baf0f139b5b2ce5c74fe43f23db3c9df09762f57
trOyYeyY <troyzuwara@gmail.com>
2024-04-02 00:50:48 +0000

    pycodestyle task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,10 +12,11 @@ def get_peak(intList, first, last):
     if intList[mid - 1] < intList[mid] and intList[mid + 1] < intList[mid]:
         return intList[mid]
 
-    elif intList[mid + 1] > intList[mid] :
+    elif intList[mid + 1] > intList[mid]:
         return get_peak(intList, mid + 1, last)
     else:
         return get_peak(intList, first, mid)
+
 
 def find_peak(list_of_integers):
     """ finds the peak in the list if list is empty return None"""
@@ -23,5 +24,5 @@ def find_peak(list_of_integers):
     if list_of_integers is None or list_of_integers == []:
         return None
     last = len(list_of_integers) - 1
-    peak = get_peak(list_of_integers, 0 , last)
+    peak = get_peak(list_of_integers, 0, last)
     return peak
