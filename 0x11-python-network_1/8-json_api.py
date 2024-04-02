e6994b24596858122bd00d2b79106f560f74056c
trOyYeyY <troyzuwara@gmail.com>
2024-04-02 01:15:14 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    q = {"q": "" if len(argv) == 1 else argv[1]}
+    request = requests.post("http://0.0.0.0:5000/search_user", data=q)
+    if '{' and '}' in request.text:
+        json = request.json()
+        if len(json) == 0:
+            print ("No result")
+        else:
+            print(f"[{json.get('id')}] {json.get('name')}")
