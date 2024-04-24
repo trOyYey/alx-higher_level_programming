ebbf3b346ed4f42b37039d48c8365feb26e1e540
trOyYey <troyzuwara@gmail.com>
2024-04-24 21:41:05 +0000

    advanced task 1

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const RQ = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + arg[2];
+RQ(url, (err, res, body) => {
+  if (err) return console.log(err);
+  const result = {};
+  JSON.parse(body).characters.forEach(charact => {
+    RQ(charact, (error, resp, bd) => {
+      if (error) return console.log(error);
+	    console.log(JSON.parse(bd).name);
+	});
+  });
+});
