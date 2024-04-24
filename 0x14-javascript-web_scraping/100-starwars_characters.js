444b99bd5bcd039747f24e943632cfd4e0b71cf7
trOyYey <troyzuwara@gmail.com>
2024-04-24 21:42:35 +0000

    fixing semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,11 +6,10 @@ const RQ = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + arg[2];
 RQ(url, (err, res, body) => {
   if (err) return console.log(err);
-  const result = {};
   JSON.parse(body).characters.forEach(charact => {
     RQ(charact, (error, resp, bd) => {
       if (error) return console.log(error);
-	    console.log(JSON.parse(bd).name);
-	});
+      console.log(JSON.parse(bd).name);
+    });
   });
 });
