bff4fd1c74e28192683a9f90fc7877c6c574d467
trOyYey <troyzuwara@gmail.com>
2024-04-29 03:41:23 +0200

    Delete 0x14-javascript-web_scraping directory
    defer

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const arg = process.argv;
-const RQ = require('request');
-
-RQ.get(arg[2]).on('response', (res) => {
-  console.log('code: ' + res.statusCode);
-});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const arg = process.argv;
-const RQ = require('request');
-
-RQ('https://swapi-api.alx-tools.com/api/films/' + arg[2], (err, res, body) => {
-  if (err) console.log(err);
-  console.log(JSON.parse(body).title);
-});
